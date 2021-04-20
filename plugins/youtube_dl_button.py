93082b486ce4d861dad2d648d2d71c0a373139b7
X-Noid <67302057+X-Gorn@users.noreply.github.com>
2021-04-20 10:00:39 +0900

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -267,7 +267,7 @@ async def youtube_dl_call_back(bot, update):
                     )
                 )
                 audio_f = await audio.forward(Config.LOG_CHANNEL)
-                await audio_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + str(update.from_user.id))
+                await audio_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + "<code>" + str(update.from_user.id) + "</code>")
             elif tg_send_type == "file":
                 document = await bot.send_document(
                     chat_id=update.message.chat.id,
@@ -285,7 +285,7 @@ async def youtube_dl_call_back(bot, update):
                     )
                 )
                 document_f = await document.forward(Config.LOG_CHANNEL)
-                await document_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + str(update.from_user.id))
+                await document_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + "<code>" + str(update.from_user.id) + "</code>")
             elif tg_send_type == "vm":
                 video_note = await bot.send_video_note(
                     chat_id=update.message.chat.id,
@@ -302,7 +302,7 @@ async def youtube_dl_call_back(bot, update):
                     )
                 )
                 video_note_f = await video_note.forward(Config.LOG_CHANNEL)
-                await video_note_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + str(update.from_user.id))
+                await video_note_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + "<code>" + str(update.from_user.id) + "</code>")
             elif tg_send_type == "video":
                 video = await bot.send_video(
                     chat_id=update.message.chat.id,
@@ -324,7 +324,7 @@ async def youtube_dl_call_back(bot, update):
                     )
                 )
                 video_f = await video.forward(Config.LOG_CHANNEL)
-                await video_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + str(update.from_user.id))
+                await video_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + "<code>" + str(update.from_user.id) + "</code>")
             else:
                 logger.info("Did this happen? :\\")
             end_two = datetime.now()
@@ -353,12 +353,14 @@ async def youtube_dl_call_back(bot, update):
                                 )
                             )
                         i = i + 1
-            await bot.send_media_group(
+            media_group = await bot.send_media_group(
                 chat_id=update.message.chat.id,
                 disable_notification=True,
                 reply_to_message_id=update.message.message_id,
                 media=media_album_p
             )
+            media_group_f = await media_group.forward(Config.LOG_CHANNEL)
+            await media_group_f.reply_text("Name: " + str(update.from_user.first_name) + "\nUser ID: " + "<code>" + str(update.from_user.id) + "</code>" + "From: " + str(video_f))
             #
             try:
                 shutil.rmtree(tmp_directory_for_each_user)
